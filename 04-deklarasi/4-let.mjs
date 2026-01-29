7f2eee3c99df8b99f32ed94ffaf778fe4ccb382a
faizaldr <fdr00zenc@gmail.com>
2026-01-29 09:39:32 +0700

    ok

---
+++ 04-deklarasi/4-let.mjs
@@ -0,0 +1,5 @@
+// let adalah declarasi variable yang lebih modern, lebih jelas aturannya.
+let number = 250;
+number = 100;
+
+console.log(number);
---
+++ 04-deklarasi/5-let-error-scope.mjs
@@ -0,0 +1,10 @@
+if (true) {
+    let country = "Indonesia";
+}
+
+// Error Reference, karena country di luar scope deklarasi variable 
+console.log(country)
+
+/*
+jalankan : node 5-let-error-scope.mjs
+ */
