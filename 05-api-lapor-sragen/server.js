6131219a371079ba95162ba6ce52c46ac58cf31b
faizaldr <fdr00zenc@gmail.com>
2026-01-30 14:19:53 +0700

    ok

--- 05-api-lapor-sragen/server.js
+++ 05-api-lapor-sragen/server.js
@@ -4,6 +4,14 @@ const express = require("express");
 const mainApp = require("./app");
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    next()
+})
+
 mainApp(app);
 
 app.listen(3000, /*callback function */() => {
---
+++ 06-fe-lapor-sragen/README.md
@@ -0,0 +1,3 @@
+1. VSC extension : live server
+
+2. jalankan : Alt + L O
